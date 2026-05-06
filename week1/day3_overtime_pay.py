ee5fa3cc7d41706adaec9120251607f754e671d7
Abdishakur Ahmed <engshakircade333@gmail.com>
2026-05-06 19:27:19 +0300

    Overtime pay calculator using conditional logic

---
+++ week1/day3_overtime_pay.py
@@ -0,0 +1,10 @@
+hours = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+
+if hours > 40:
+    overtime_hours = hours - 40
+    gross_pay = (40 * rate) + (overtime_hours * rate * 1.5)
+else:
+    gross_pay = hours * rate
+
+print("Gross Pay:", gross_pay)
