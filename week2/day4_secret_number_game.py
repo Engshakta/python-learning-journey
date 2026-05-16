d363428dd4a1374c9f28cc10fcf2de9079829b09
Abdishakur Ahmed <engshakircade333@gmail.com>
2026-05-16 22:27:33 +0300

    Week 2 Day 5: Implemented secret exit loop using break

--- week2/day4_secret_number_game.py
+++ week2/day4_secret_number_game.py
@@ -1,18 +1,23 @@
-secret_number = 777
-print(
-    """
-    Welcome to my game, muggle!    
-| Enter an integer number       
-| and guess what number I've    
-| picked for you.                
-| So, what is the secret number?
-    """
-)
+# secret_number = 777
+# print(
+#     """
+#     Welcome to my game, muggle!    
+# | Enter an integer number       
+# | and guess what number I've    
+# | picked for you.                
+# | So, what is the secret number?
+#     """
+# )
 
-while True :
-    number = int(input("Enter a number to gues the secret number "))
-    if number == secret_number:
-        print("Well done, muggle! You are free now")
-        break
-    else:
-        print("Ha ha! You're stuck in my loop!")
+# while True :
+#     number = int(input("Enter a number to gues the secret number "))
+#     if number == secret_number:
+#         print("Well done, muggle! You are free now")
+#         break
+#     else:
+#         print("Ha ha! You're stuck in my loop!")
+
+
+for i in range(2, 8):
+    print("The value of i is currently", i)
+
